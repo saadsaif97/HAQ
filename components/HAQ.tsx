7ce60a26a4f700f6fbb6cd651cac949199e01b1f
saadgfx97 <saadgfx97@gmail.com>
2024-01-26 12:51:52 +0500

    api end point

--- components/HAQ.tsx
+++ components/HAQ.tsx
@@ -4,6 +4,36 @@ import React, { useEffect } from 'react'
 import { createWidget } from '@typeform/embed'
 import '@typeform/embed/build/css/widget.css'
 
+
+async function fetchResponse(formId: string, responseId: string) {
+  const accessToken = 'tfp_GedqovrmeL99YUrEBR8etQXut8jdYMer1eXBaqezTxqW_3peGw8t3j6cdAN';
+
+  const apiUrl = `https://api.typeform.com/forms/${formId}/responses?response_id=${responseId}`;
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: 'GET',
+      headers: {
+        'Authorization': `bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    // Process the response data
+    console.log({data});
+
+    return data; // You can return the data if needed
+  } catch (error) {
+    console.error('Fetch error:', error);
+    throw error; // Rethrow the error if needed
+  }
+}
+
 export default function HAQ() {
 
   useEffect(()=>{
@@ -11,7 +41,11 @@ export default function HAQ() {
       container: document.querySelector('#form'),
       width: 1068,
       height: 600,
-      fullScreen: true
+      fullScreen: true,
+      async onSubmit(event) {
+        await fetchResponse(event.formId, event.responseId)
+        console.log({event})
+      },
     })
   }, [])
 
