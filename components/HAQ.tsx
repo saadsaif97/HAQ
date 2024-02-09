c4dbc0b438243e531a1199a1abed0c30ccf243e1
saadgfx97 <saadgfx97@gmail.com>
2024-02-09 20:15:23 +0500

    get email

--- components/HAQ.tsx
+++ components/HAQ.tsx
@@ -16,10 +16,34 @@ export default function HAQ() {
       height: 600,
       async onSubmit(event) {
         // @TODO: get email and send pdf in attachment
+        const email =  await getEmail(event.responseId);
+        console.log({email})
         router.push(`/response?responseId=${event.responseId}`)
       },
     });
   }, []);
 
   return <div id="form"></div>;
+}
+
+async function getEmail(responseId: string | null | undefined) {
+  if (!responseId) return;
+
+  try {
+    const res = await fetch("http://localhost:3000/api/responseEmail", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        formId: "UWy7JY9v",
+        responseId: responseId,
+      }),
+    });
+
+    return await res.json();
+  } catch (error) {
+    console.log({error});
+    return (error)
+  }
 }
---
+++ pages/api/responseEmail.ts
@@ -0,0 +1,60 @@
+
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type ResponseData = {
+  error?: unknown;
+  email?: unknown;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  try {
+    const formId = req.body?.formId;
+    const responseId = req.body?.responseId;
+
+    if (!formId || !responseId) {
+      res
+        .status(400)
+        .json({ error: "Form id and response id both are required" });
+    }
+
+    const variables = await fetchResponse(formId, responseId);
+    res.status(200).json(variables);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+}
+
+async function fetchResponse(formId: string, responseId: string) {
+  const apiUrl = `https://api.typeform.com/forms/${formId}/responses?included_response_ids=${responseId}`;
+
+  let token = process.env.TYPE_FORM_ACESS_TOKEN;
+
+  if (!token)
+    return {
+      error: `No token found with key: process.env.TYPE_FORM_ACESS_TOKEN: ${token}`,
+    };
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        Authorization: `bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    return data.items[0].answers.find(({type}) => type=="email").email;
+  } catch (error) {
+    console.error("Fetch error:", error);
+    throw error; // Rethrow the error if needed
+  }
+}
