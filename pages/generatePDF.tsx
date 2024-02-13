edaa99123ba49dc61a02348c7edfd370fbf08c65
saadgfx97 <saadgfx97@gmail.com>
2024-02-13 19:48:40 +0500

    installed puppeteer

--- pages/generatePDF.tsx
+++ pages/generatePDF.tsx
@@ -5,7 +5,7 @@ export default function generatePDF() {
   async function createPDF() {
     try {
       const email = await getEmail("f0a4hhhhd1gr5dmujf0a4hf6ue63jvy4");
-      console.log({email})
+      console.log({ email });
       const res = await fetch("/api/generatePDF", {
         method: "POST",
         headers: {
@@ -14,11 +14,11 @@ export default function generatePDF() {
         body: JSON.stringify({
           formId: "UWy7JY9v",
           responseId: "f0a4hhhhd1gr5dmujf0a4hf6ue63jvy4",
-          email: "msaadbinsaif@gmail.com"
+          email: "msaadbinsaif@gmail.com",
         }),
       });
-      const data = await res.json()
-      console.log({data})
+      const data = await res.json();
+      console.log({ data });
     } catch (error) {
       console.log({ error });
     }
@@ -35,20 +35,23 @@ async function getEmail(responseId: string | null | undefined) {
   if (!responseId) return;
 
   try {
-    const res = await fetch("https://unit203-haq.vercel.app/api/responseEmail", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        formId: "UWy7JY9v",
-        responseId: responseId,
-      }),
-    });
+    const res = await fetch(
+      "https://unit203-haq.vercel.app/api/responseEmail",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          formId: "UWy7JY9v",
+          responseId: responseId,
+        }),
+      }
+    );
 
     return await res.json();
   } catch (error) {
-    console.log({error});
-    return (error)
+    console.log({ error });
+    return error;
   }
 }
