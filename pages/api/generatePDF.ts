2d15c7fb22f8bbfd64802bacb327b3b561554ca7
saadgfx97 <saadgfx97@gmail.com>
2024-02-16 01:48:28 +0500

    format

--- pages/api/generatePDF.ts
+++ pages/api/generatePDF.ts
@@ -21,7 +21,7 @@ async function getBrowser() {
 async function createPDF(responseId: string) {
   try {
     const URL =
-      `http://localhost:3000/response?responseId=${responseId}`;
+      `https://haq-unit-203.vercel.app/response?responseId=${responseId}`;
 
     const browser = await getBrowser();
     const page = await browser.newPage();
