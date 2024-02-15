7d922a21ebcc1e3d7b6597dbc916094fd56b4bce
saadgfx97 <saadgfx97@gmail.com>
2024-02-15 20:11:02 +0500

    created PDF

--- pages/api/generatePDF.ts
+++ pages/api/generatePDF.ts
@@ -15,6 +15,9 @@ async function getBrowser() {
     headless: true,
     channel: 'chrome',
     args: ['--hide-scrollbars', '--disable-web-security'],
+    executablePath: await chromium.executablePath(
+      `https://github.com/Sparticuz/chromium/releases/download/v121.0.0/chromium-v121.0.0-pack.tar`
+    ),
   });
 }
 
