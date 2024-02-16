3e70e1417f4cd92caa0e359250e3516a83314d99
saadgfx97 <saadgfx97@gmail.com>
2024-02-16 15:03:50 +0500

    post install

--- pages/api/generatePDF.ts
+++ pages/api/generatePDF.ts
@@ -23,7 +23,7 @@ async function getBrowser() {
     channel: "chrome",
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath(
-      `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
+      `https://github.com/Sparticuz/chromium/releases/download/v121.0.0/chromium-v121.0.0-pack.tar`
     ),
     ignoreHTTPSErrors: true,
   });
